b1f1c622e4d8b4ebaed961e6ad0b1e872aecc334
Sam Chillcott <sam@samchillcott.com>
2020-09-29 11:25:19 +0100

    useEffect setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import {useForm} from "./useForm";
+import { useForm } from "./useForm";
 
 function App() {
-	const [values, handleChange] = useForm({email: "", password: ""});
+	const [values, handleChange] = useForm({
+		email: "",
+		password: "",
+		firstName: "",
+	});
 
 	console.log(values.email);
 	console.log(values.password);
+
+	useEffect(() => {
+		console.log("render");
+	}, [values.email, values.password]);
+
 	return (
 		<div>
+			<input name="email" placeholder="email" value={values.email} onChange={handleChange} />
 			<input
-				name="email"
-				value={values.email}
+        name="firstName"
+        placeholder="First Name"
+				value={values.firstName}
 				onChange={handleChange}
 			/>
 			<input
 				type="password"
-				name="password"
+        name="password"
+        placeholder="Password"
 				value={values.password}
 				onChange={handleChange}
 			/>
