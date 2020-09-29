e8ade8e32eda5f15a32aa4a598f78bb90fda6c12
Sam Chillcott <sam@samchillcott.com>
2020-09-29 12:17:57 +0100

    multiple useEffects

--- src/App.js
+++ src/App.js
@@ -13,22 +13,43 @@ function App() {
 	console.log(values.password);
 
 	useEffect(() => {
-		console.log("render");
-	}, [values.email, values.password]);
+		const onMouseMove = (e) => {
+			console.log(e);
+		};
+
+		window.addEventListener("mousemove", onMouseMove);
+
+		return () => {
+			window.removeEventListener("mousemove", onMouseMove);
+		};
+	}, [values.email]);
+
+	// useEffect(() => {
+	// 	console.log("mount1");
+	// }, []);
+
+	// useEffect(() => {
+	// 	console.log("mount2");
+	// }, []);
 
 	return (
 		<div>
-			<input name="email" placeholder="email" value={values.email} onChange={handleChange} />
 			<input
-        name="firstName"
-        placeholder="First Name"
+				name="email"
+				placeholder="email"
+				value={values.email}
+				onChange={handleChange}
+			/>
+			<input
+				name="firstName"
+				placeholder="First Name"
 				value={values.firstName}
 				onChange={handleChange}
 			/>
 			<input
 				type="password"
-        name="password"
-        placeholder="Password"
+				name="password"
+				placeholder="Password"
 				value={values.password}
 				onChange={handleChange}
 			/>
