a8ec93863d30fa6ad0d6866f026664bb2a8c1201
Sam Chillcott <sam@samchillcott.com>
2020-10-03 13:03:55 +0100

    eop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { Hello } from "./Hello";
+// import { Hello } from "./Hello";
+import { useFetch } from "./useFetch";
 import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const {data} = useFetch("https://api.kanye.rest");
 
 	return (
 		<div>
-			<Hello increment={() => setCount(count + 1)} />
 			<div>Count: {count}</div>
+			<div>{data}</div>
+			<button onClick={() => setCount(count + 1)}>Increment</button>
 		</div>
 	);
 }
