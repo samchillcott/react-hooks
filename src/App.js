db71b5b1b1efaf8dbcd968570ef2d63e65f4bba5
Sam Chillcott <sam@samchillcott.com>
2020-10-03 09:20:28 +0100

    useCallback setup

--- src/App.js
+++ src/App.js
@@ -1,63 +1,14 @@
-import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
+import React from "react";
+import Helo from "./Hello"
 import "./App.css";
-import { useForm } from "./useForm";
-import { useFetch } from "./useFetch";
+
 
 function App() {
-	const [values, handleChange] = useForm({
-		email: "",
-		password: "",
-		firstName: "",
-	});
 
-	const [count, setCount] = useState(() =>
-		JSON.parse(localStorage.getItem("count"))
-	);
-	const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-
-	useEffect(() => {
-		localStorage.setItem("count", JSON.stringify(count));
-	}, [count]);
-
-	const inputRef = useRef();
-
-	// useLayoutEffect(() => {
-	// 	console.log(inputRef.current.getBindingClientRect());
-	// }, []);
 
 	return (
 		<div>
-			<div>{!data ? "loading..." : data}</div>
-			<div>count: {count} </div>
-			<button onClick={() => setCount((c) => c + 1)}>Increment</button>
-
-			<input
-				ref={inputRef}
-				name="email"
-				placeholder="email"
-				value={values.email}
-				onChange={handleChange}
-			/>
-			<input
-				name="firstName"
-				placeholder="First Name"
-				value={values.firstName}
-				onChange={handleChange}
-			/>
-			<input
-				type="password"
-				name="password"
-				placeholder="Password"
-				value={values.password}
-				onChange={handleChange}
-			/>
-			<button
-				onClick={() => {
-					inputRef.current.focus();
-				}}
-			>
-				Focus
-			</button>
+			<Hello />
 		</div>
 	);
 }
